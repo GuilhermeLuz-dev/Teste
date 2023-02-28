eaa2f9ce6ca5b56ca6f3f7915db6da5f18ec8e45
Guilherme Luz Rocha <115510754+GuilhermeLuz-dev@users.noreply.github.com>
2023-02-27 22:01:42 -0300

    Update script.js

--- Frequência de Fibonacci/script.js
+++ Frequência de Fibonacci/script.js
@@ -4,21 +4,19 @@ let aux2 = 1;
 let numeroEncontrado = false;
 
 function calcSeqFibonacci() {
-    while(!numeroEncontrado) {
-         
+    while (!numeroEncontrado) {
+
         aux1 = aux1 + aux2;
         aux2 = aux1 + aux2;
-        
+
         if (aux1 == numInformado || aux2 == numInformado) {
             numeroEncontrado = true;
             console.log(`O número ${numInformado} pertence a sequência de Fibonacci.`);
-            
+
         } else if (aux1 > numInformado || aux2 > numInformado) {
             numeroEncontrado = true;
             console.log(`O número ${numInformado} não pertence a sequência de Fibonacci`);
         }
-
-       
     }
 }
 
